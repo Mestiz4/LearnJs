e700e70396951421c400c66677b49ea7d5852cbd
Mestiz4 <info.mestiz4@gmail.com>
2023-04-28 15:19:47 -0500

    conceptos basicos

---
+++ Js/index.js
@@ -0,0 +1,7 @@
+/* 
+    *Js es sensitivo a las mayusc. por eso hay que estar pendientes de que las funciones y demás variables cuando se estén llamando se hagan de la manera adecuada
+    *para hacer comentarios se deben hacer entre los eslash y el asterisco como este o también con dos slash  
+    
+    */
+
+    //este es un comentario de una sola  línea
