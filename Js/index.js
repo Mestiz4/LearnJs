a3e012065bb01283d7b82e657430bd121a4b30f4
Mestiz4 <info.mestiz4@gmail.com>
2023-05-02 20:52:53 -0500

    variables

--- Js/index.js
+++ Js/index.js
@@ -1,7 +1,12 @@
 /* 
     *Js es sensitivo a las mayusc. por eso hay que estar pendientes de que las funciones y demás variables cuando se estén llamando se hagan de la manera adecuada
-    *para hacer comentarios se deben hacer entre los eslash y el asterisco como este o también con dos slash  
+    *para hacer comentarios se deben hacer entre los eslash y el asterisco como este o también con dos slash 
+    *para nombrar funciones y demás se debe usar el camelCase, que quiere decir escribir la primera letra en minúscula y las demás palabras iniciarlas con alta
+    ejemplo de ello: myCart / doSomethingImportant
+    !PALABRAS RESERVADAS EN ECMAScript
+    await / break / case / catch / class / const / continue / debugger / default / delete / do / else / export / 
+    extends / finally / for / function / if / import / in / instanceof / new / return / super / switch / 
+    this / throw / try / typeof / var / void / while / with / yield */
     
-    */
 
     //este es un comentario de una sola  línea
